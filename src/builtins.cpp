228f730343623959a122755ce9f5e0451c00ee35
Mazen <mazentamer2007@gmail.com>
2026-06-19 17:23:14 +0300

    Added history

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -6,6 +6,17 @@
 #include <iostream>
 
 bool Builtins::handle(const std::vector<std::string>& tokens) {
+
+  static std::vector<std::string> history;
+  std::string result;
+
+  for (size_t i = 0; i < tokens.size(); i++) {
+    if (i > 0) result += ' ';
+    result += tokens[i];
+  }
+
+  history.push_back(result);
+
   if (tokens[0] == "exit") {
     std::exit(EXIT_SUCCESS);
   } else if (tokens[0] == "cd") {
@@ -30,6 +41,11 @@ bool Builtins::handle(const std::vector<std::string>& tokens) {
       }
     }
 
+    return true;
+  } else if (tokens[0] == "history") {
+    for (size_t i = 0; i < history.size(); i++) {
+      std::cout << i + 1 << " " << history[i] << std::endl;
+    }
     return true;
   }
 
